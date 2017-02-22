f15786c0da52d0ce49bc6398dd9dea5677319792
azr <adrien.delorme@icloud.com>
2017-02-22 15:52:27 +0100

    update to use with v3 + simplification + less is more + docs
    * added possibility to set custom labels instead of forcing ‘source’
      * explained labels a bit
    * now have 2 main components, Config & Report which give enough leverage
    * removed useless public methods
    * added doc and examples

--- gcm.go
+++ gcm.go
@@ -13,21 +13,28 @@ import (
 	"sync"
 	"time"
 
-	"google.golang.org/api/cloudmonitoring/v2beta2"
+	cloudmonitoring "google.golang.org/api/monitoring/v3"
 
 	"github.com/rcrowley/go-metrics"
 )
 
-//Reporter reports your metrics to GC monitoring
-type Reporter struct {
-	Registry metrics.Registry         // what metrics to report ?
-	Interval time.Duration            // report every ?
-	gcms     *cloudmonitoring.Service // report to that service
-	project  string
-	source   string
-
-	gauges map[string]struct{}
-	//a fast and simple cache
+//Config contains configration to reports metrics to gcm
+//
+//Look at the Monitor func for an example usage.
+type Config struct {
+	Service *cloudmonitoring.Service // report to that service
+	//Project measured.
+	//you might want to set it to `projects/` + project name
+	//when creating the reporter manually
+	Project string
+	//Labels to send along with queries,
+	//usefull to know who sent what ex: ["source":$HOSTNAME].
+	//labels are created on the first query,
+	//if you start with no label then add some,
+	//gcm will complain.
+	//if you add a label and then remove one also
+	//recreate metric to fix this
+	Labels map[string]string
 }
 
 var (
@@ -35,188 +42,176 @@ var (
 	histogramsNotImplemented sync.Once
 )
 
-//NewReporter instantiates a reporter that sends metrics to GC monitoring
-//source will be set labels as ["0":source]
-//to help identify the machine sending the data
-func NewReporter(r metrics.Registry, i time.Duration, s *cloudmonitoring.Service, project, source string) *Reporter {
-	return &Reporter{
-		Registry: r,
-		Interval: i,
-		gcms:     s,
-		project:  project,
-		source:   source,
-		gauges:   make(map[string]struct{}),
+//Monitor starts a new single threaded monitoring process.
+//
+//Example:
+//		client, err := google.DefaultClient(ctx, cloudmonitoring.MonitoringScope)
+//		if err != nil {
+//			log.Fatal("Failed to get cloudmonitoring default client:", err)
+//		}
+//		s, err := cloudmonitoring.New(client)
+//		if err != nil {
+//			log.Fatal("Failed to get cloudmonitoring default service:", err)
+//		}
+//
+//		hostname, _ := os.Hostname()
+//		if hostname == "" {
+//			hostname = "unknown-hostname"
+//		}
+//		go googlecloudmetrics.Monitor(metrics.DefaultRegistry, 3*time.Second, s, gcpProject, map[string]string{"source": hostname})
+func Monitor(r metrics.Registry, tick time.Duration, s *cloudmonitoring.Service, project string, labels map[string]string) {
+	reporter := Config{
+		Service: s,
+		Project: "projects/" + project,
+	}
+	for range time.Tick(tick) {
+		reporter.Report(r)
 	}
 }
 
-//Monitor starts a new
-func Monitor(r metrics.Registry, i time.Duration, s *cloudmonitoring.Service, project, source string) {
-	NewReporter(r, i, s, project, source).Run()
-}
+//Report every metric from r to gcm
+func (config *Config) Report(r metrics.Registry) {
+	now := time.Now()
+	reqs, err := config.buildTimeSeries(now.Format(time.RFC3339), r)
+	if err != nil {
+		log.Printf("ERROR sending gcm request %s", err)
+		return
+	}
 
-func (self *Reporter) Run() {
-	ticker := time.Tick(self.Interval)
-	tss := cloudmonitoring.NewTimeseriesService(self.gcms)
-	before := time.Now()
-	for now := range ticker {
-		now = time.Now()
-		reqs, err := self.BuildRequests(now.Format(time.RFC3339), before.Format(time.RFC3339), self.Registry)
-		before = now
-		if err != nil {
-			log.Printf("ERROR sending gcm request %s", err)
-			continue
-		}
-
-		for _, req := range reqs {
-			wr := &cloudmonitoring.WriteTimeseriesRequest{
-				CommonLabels: map[string]string{
-				// "0": "source",
-				},
-			}
-			req.TimeseriesDesc.Labels = map[string]string{
-			// "source": DotSlashes(self.source),
-			}
-			//Can only write one simple point at a time !
-			wr.Timeseries = append(wr.Timeseries, req)
-			_, err = tss.Write(self.project, wr).Do()
-			if err != nil {
-				log.Printf("ERROR sending metrics to gcm %s", err)
-			}
-		}
+	wr := &cloudmonitoring.CreateTimeSeriesRequest{
+		TimeSeries: reqs,
+	}
+	_, err = cloudmonitoring.NewProjectsTimeSeriesService(config.Service).Create(config.Project, wr).Do()
+	if err != nil {
+		log.Printf("ERROR sending metrics to gcm %s", err)
 	}
 }
 
+//DotSlashes makes golang metrics look like gcm metrics.
+//
+//ex : runtime.MemStats.StackSys -> runtime/MemStats/StackSys
 func DotSlashes(name string) string {
 	return strings.Replace(name, ".", "/", -1)
 }
-func NamespacedName(name string) string {
-	return fmt.Sprintf("custom.cloudmonitoring.googleapis.com/%s", DotSlashes(name))
+
+//customMetric names your custom metric
+func customMetric(name string) string {
+	return fmt.Sprintf("custom.googleapis.com/%s", DotSlashes(name))
 }
 
-//https://cloud.google.com/monitoring/demos/wrap_write_lightweight_metric
-//https://cloud.google.com/monitoring/api/metrics
-func (self *Reporter) BuildRequests(
-	start, end string,
-	r metrics.Registry,
-) (
-	pts []*cloudmonitoring.TimeseriesPoint,
-	err error,
-) {
+//newTimeSeries creates a time serie named 'name' with labels from Config.
+func (config *Config) newTimeSeries(name string) *cloudmonitoring.TimeSeries {
+	return &cloudmonitoring.TimeSeries{
+		Metric: &cloudmonitoring.Metric{
+			Type:   customMetric(name),
+			Labels: config.Labels,
+		},
+		// ValueType: "int64",
+		Resource: &cloudmonitoring.MonitoredResource{
+			Type: "global", // a good default
+		},
+	}
+}
+
+//buildTimeSeries returns an array of cloudmonitoring.TimeSeries containing
+//eery metric to send to gcm.
+func (config *Config) buildTimeSeries(start string, r metrics.Registry) (pts []*cloudmonitoring.TimeSeries, err error) {
 	r.Each(func(name string, metric interface{}) {
-		p := &cloudmonitoring.TimeseriesPoint{
-			TimeseriesDesc: &cloudmonitoring.TimeseriesDescriptor{
-				Metric:  NamespacedName(name),
-				Project: self.project,
-			},
-			Point: &cloudmonitoring.Point{
-				Start: start,
-				End:   end,
-			},
-		}
 		switch m := metric.(type) {
 		case metrics.Counter:
 			v := m.Count()
 			if v == 0 {
 				return
 			}
-			p.Point.Int64Value = &v
-			p.Point.Start = end
+			p := config.newTimeSeries(name)
+			p.Points = append(p.Points, &cloudmonitoring.Point{
+				Value: &cloudmonitoring.TypedValue{
+					Int64Value: &v,
+				},
+				Interval: &cloudmonitoring.TimeInterval{
+					EndTime: start,
+				},
+			})
 			pts = append(pts, p)
 		case metrics.Gauge:
 			v := m.Value()
-			m = m.Snapshot()
 			if v == 0 {
 				return
 			}
-			self.CreateGauge(name, "int64")
-			p.Point.Int64Value = &v
-			p.Point.Start = end
+			p := config.newTimeSeries(name)
+			p.Points = append(p.Points, &cloudmonitoring.Point{
+				Value: &cloudmonitoring.TypedValue{
+					Int64Value: &v,
+				},
+				Interval: &cloudmonitoring.TimeInterval{
+					EndTime: start,
+				},
+			})
 			pts = append(pts, p)
 		case metrics.GaugeFloat64:
 			v := m.Value()
 			if v == 0 {
 				return
 			}
-			self.CreateGauge(name, "double")
-			p.Point.DoubleValue = &v
-			p.Point.Start = end
+			p := config.newTimeSeries(name)
+			p.Points = append(p.Points, &cloudmonitoring.Point{
+				Value: &cloudmonitoring.TypedValue{
+					DoubleValue: &v,
+				},
+				Interval: &cloudmonitoring.TimeInterval{
+					EndTime: start,
+				},
+			})
 			pts = append(pts, p)
 		case metrics.Meter:
 			m = m.Snapshot()
-			if v := m.Count(); v != 0 {
-				pts = append(pts,
-					&cloudmonitoring.TimeseriesPoint{
-						TimeseriesDesc: &cloudmonitoring.TimeseriesDescriptor{
-							Metric:  NamespacedName(name + ".count"),
-							Project: self.project,
-						},
-						Point: &cloudmonitoring.Point{
-							Start:      end,
-							End:        end,
-							Int64Value: &v,
-						},
-					},
-				)
-			}
 			if v := m.RateMean(); v != 0 {
-				pts = append(pts,
-					&cloudmonitoring.TimeseriesPoint{
-						TimeseriesDesc: &cloudmonitoring.TimeseriesDescriptor{
-							Metric:  NamespacedName(name + ".mean"),
-							Project: self.project,
-						},
-						Point: &cloudmonitoring.Point{
-							Start:       end,
-							End:         end,
-							DoubleValue: &v,
-						},
+				p := config.newTimeSeries(name + ".mean")
+				p.Points = append(p.Points, &cloudmonitoring.Point{
+					Value: &cloudmonitoring.TypedValue{
+						DoubleValue: &v,
 					},
-				)
+					Interval: &cloudmonitoring.TimeInterval{
+						EndTime: start,
+					},
+				})
+				pts = append(pts, p)
 			}
 			if v := m.Rate1(); v != 0 {
-				pts = append(pts,
-					&cloudmonitoring.TimeseriesPoint{
-						TimeseriesDesc: &cloudmonitoring.TimeseriesDescriptor{
-							Metric:  NamespacedName(name + ".1min"),
-							Project: self.project,
-						},
-						Point: &cloudmonitoring.Point{
-							Start:       end,
-							End:         end,
-							DoubleValue: &v,
-						},
+				p := config.newTimeSeries(name + ".1min")
+				p.Points = append(p.Points, &cloudmonitoring.Point{
+					Value: &cloudmonitoring.TypedValue{
+						DoubleValue: &v,
 					},
-				)
+					Interval: &cloudmonitoring.TimeInterval{
+						EndTime: start,
+					},
+				})
+				pts = append(pts, p)
 			}
 			if v := m.Rate5(); v != 0 {
-				pts = append(pts,
-					&cloudmonitoring.TimeseriesPoint{
-						TimeseriesDesc: &cloudmonitoring.TimeseriesDescriptor{
-							Metric:  NamespacedName(name + ".5min"),
-							Project: self.project,
-						},
-						Point: &cloudmonitoring.Point{
-							Start:       end,
-							End:         end,
-							DoubleValue: &v,
-						},
+				p := config.newTimeSeries(name + ".5min")
+				p.Points = append(p.Points, &cloudmonitoring.Point{
+					Value: &cloudmonitoring.TypedValue{
+						DoubleValue: &v,
 					},
-				)
+					Interval: &cloudmonitoring.TimeInterval{
+						EndTime: start,
+					},
+				})
+				pts = append(pts, p)
 			}
 			if v := m.Rate15(); v != 0 {
-				pts = append(pts,
-					&cloudmonitoring.TimeseriesPoint{
-						TimeseriesDesc: &cloudmonitoring.TimeseriesDescriptor{
-							Metric:  NamespacedName(name + ".15min"),
-							Project: self.project,
-						},
-						Point: &cloudmonitoring.Point{
-							Start:       end,
-							End:         end,
-							DoubleValue: &v,
-						},
+				p := config.newTimeSeries(name + ".15min")
+				p.Points = append(p.Points, &cloudmonitoring.Point{
+					Value: &cloudmonitoring.TypedValue{
+						DoubleValue: &v,
 					},
-				)
+					Interval: &cloudmonitoring.TimeInterval{
+						EndTime: start,
+					},
+				})
+				pts = append(pts, p)
 			}
 		case metrics.Histogram:
 			if m.Count() > 0 {
@@ -235,29 +230,4 @@ func (self *Reporter) BuildRequests(
 		}
 	})
 	return
-}
-
-func (r *Reporter) CreateGauge(name, valueType string) {
-	if _, found := r.gauges[name]; found {
-		return
-	}
-	g := &cloudmonitoring.MetricDescriptor{
-		Name: NamespacedName(name),
-		Labels: []*cloudmonitoring.MetricDescriptorLabelDescriptor{
-			&cloudmonitoring.MetricDescriptorLabelDescriptor{
-				Description: "",
-				Key:         NamespacedName(name),
-			},
-		},
-		TypeDescriptor: &cloudmonitoring.MetricDescriptorTypeDescriptor{
-			MetricType: "gauge",
-			ValueType:  valueType,
-		},
-		Project:     r.project,
-		Description: "Created by go-metrics",
-	}
-	_, err := r.gcms.MetricDescriptors.Create(r.project, g).Do()
-	if err != nil {
-		r.gauges[name] = struct{}{}
-	}
 }
